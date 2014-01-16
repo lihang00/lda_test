67d8d608dca0faf4a7afc92da8e1c064eba3c02f
Soundwave <lihang00@gmail.com>
2014-01-15 23:12:12 -0800

    add sample part.

--- estimator.go
+++ estimator.go
@@ -47,7 +47,6 @@ func (e *Estimator) Train() {
     // init model
     z := []*IntVector{} // topic assignments for words, size M(number of docs) x doc.size() (word count in a doc)
     nd := []*IntVector{} //nd[i][j]: number of words in document i assigned to topic j, size M x K
-    nwsum := NewIntVector() //nwsum[j]: total number of words assigned to topic j, size K
     ndsum := NewIntVector() //ndsum[i]: total number of words in document i, size M
 
     for m := int64(0); m < int64(len(e.Data)); m++ {
@@ -66,7 +65,7 @@ func (e *Estimator) Train() {
             // number of words in document i assigned to topic j
             nd[m].AddValue(topic, 1);
             // total number of words assigned to topic j
-            nwsum.AddValue(topic, 1);
+            e.Model.IncreaseNZ(topic)
         }
 
         // total number of words in document i
@@ -78,12 +77,53 @@ func (e *Estimator) Train() {
     for iter := int64(0); iter < e.Param.Niters; iter++ {
         fmt.Println("Iteration ", iter, " ...")
             
-        // for all z_i
+        // for all z_i        
+        for m := int64(0); m < int64(len(e.Data)); m++ {
+            doc := e.Data[m]
+            for n := int64(0); n < int64(len(doc.Words)); n++ {
+                // z_i = z[m][n]
+                topic := z[m].GetValue(n)
+                word := doc.Words[n]
 
+                // before sample
+                e.Model.DecreaseNWZ(word, topic)
+                e.Model.DecreaseNZ(topic)
+                nd[m].AddValue(topic, -1)
+                ndsum.AddValue(m, -1)
 
-    }
+                // sample from p(z_i|z_-i, w)
+                pr := e.CalSamplePr(word, nd[m], ndsum.GetValue(m))
+                new_topic := pr.Sample()
 
+                // add newly estimated z_i to count variables
+                e.Model.IncreaseNWZ(word, new_topic)
+                e.Model.IncreaseNZ(topic)
+                nd[m].AddValue(topic, 1)
+                ndsum.AddValue(m, 1)
 
+                z[m].SetValue(n, topic)
+            }// end for each word
+        }// end for each document
+    }// end iterations  
 
+    fmt.Println("Gibbs sampling completed!");
+    fmt.Println("Saving the final model!");
+
+    // calcPerplexity
+
+    // save model
 }
 
+func (e *Estimator) CalSamplePr(word int64, ndm *IntVector, ndsumm int64) *Prod{
+    pr := NewProd()
+    Vbeta := e.Model.beta * float64(e.Model.GetWordCount())
+    Kalpha := e.Model.alpha * float64(e.Model.ntopics)
+    for k := int64(0); k < e.Model.ntopics ; k++ {
+        pr_k := (float64(e.Model.GetNWZElement(word, k)) + e.Model.beta) / (float64(e.Model.GetNZElement(k)) + Vbeta) * 
+                (float64(ndm.GetValue(k)) + e.Model.alpha) / (float64(ndsumm) + Kalpha)
+        pr.AddValue(k, pr_k)
+    }
+
+    return pr
+
+}
